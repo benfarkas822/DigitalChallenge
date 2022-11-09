2c8ca7c31dcdd532236493c57033a0bc4fe55fdd
Farkas Bence <farkas.bence@jacsomedia.hu>
2022-11-09 09:37:18 +0100

    alt tags added

--- client/src/components/UI/Card.jsx
+++ client/src/components/UI/Card.jsx
@@ -5,7 +5,8 @@ const Card = ({driver, overTake}) => {
     return (
         <div
             className={" pt-10 shadow-xl rounded-xl bg-slate-200 grid grid-cols-3 w-fit items-end justify-between hover:bg-slate-300 transition-all"}>
-            <img src={'/images/' + driver.code + '.png'} alt="" className={'w-40'}/>
+            <img src={'/images/' + driver.code + '.png'} alt={driver.firstname + ' ' + driver.lastname}
+                 className={'w-40'}/>
             < div className="flex h-full flex-col col-span-2 gap-2 mb-3">
                 <div className="text-xl">
                     Place: {driver.place}
@@ -13,7 +14,7 @@ const Card = ({driver, overTake}) => {
                 <div className="font-black">
                     {driver.firstname} {driver.lastname}
                 </div>
-                <div className=""><img src={'https://countryflagsapi.com/svg/' + driver.country} alt=""
+                <div className=""><img src={'https://countryflagsapi.com/svg/' + driver.country} alt={driver.country}
                                        className={'h-5'}/></div>
                 <div className="">{driver.code}</div>
                 <div className="font-bold">{driver.team}</div>
